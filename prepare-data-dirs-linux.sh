2944388ee448672cdcd9a79e988a8a1952a13887
ai03 <admin@ai03.com>
2026-08-10 01:29:09 +0900

    Update datadir prep script

--- prepare-data-dirs-linux.sh
+++ prepare-data-dirs-linux.sh
@@ -1,6 +1,6 @@
 sudo mkdir -p ./app_data/backend_db
 sudo mkdir -p ./app_data/backend_media
 sudo mkdir -p ./app_data/valkey_persistent
-sudo chown -R 1001:1001 ./app_data/backend_db
-sudo chown -R 1001:1001 ./app_data/backend_media
+sudo chown -R 1000:1000 ./app_data/backend_db
+sudo chown -R 1000:1000 ./app_data/backend_media
 sudo chown -R 1001:1001 ./app_data/valkey_persistent
